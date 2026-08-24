c3a6c8cf6c6e32f4b2aaeffacf53f85608c1a307
siddharthastro <siddharth12108@gmail.com>
2026-08-24 11:02:02 +0000

    cmath

---
+++ practice30.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+# include <cmath>
+int main()
+{
+    // using absolute function
+    std::cout << "abs(8) = " << abs(8) << "\n";
+    std::cout << "abs(-8) = " << abs(-8) << "\n";
+    // using floor and ceil function
+    std::cout << "ceil(12.434) = " << ceil(12.434) << "\n";
+    std::cout << "floor(12.434) = " << floor(12.434) << "\n";
+    // using log and log10
+    std::cout << "log(100) = " << log(100) << "\n";
+    std::cout << "log10(100) = " << log10(100) << "\n";
+    // using exp and pow
+    std::cout << "exp(5) = " << exp(5) << "\n";
+    std::cout << "pow (3,4) = " << pow(3,4) << "\n";
+    // sqrt fuction
+    std::cout << "sqrt(25) = "<< sqrt(25) << "\n";
+    
+    return 0;
+}
