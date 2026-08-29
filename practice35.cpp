6d31f0d12f11b350a7d5102556eba00a2c9f5861
siddharthastro <siddharth12108@gmail.com>
2026-08-29 13:40:43 +0000

    using void function

---
+++ practice35.cpp
@@ -0,0 +1,16 @@
+// defining a void function which prints a greeting 
+# include <iostream>
+
+void greeting()
+{
+    std::cout << "**************************" << "\n";
+    std::cout << "*      HELLO MASTER      *" << "\n";
+    std::cout << "**************************" << "\n";
+    return;
+}
+
+int main()
+{
+    greeting();
+    return 0;
+}
